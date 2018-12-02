01db1a1e2f2c8e83518e93435a11763d5bb475f5
willPanwj <panweijie34@163.com>
2018-12-02 21:46:57 +0800

    删除多余空格

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
           <p>git branch master: commit 2</p>
           <p>git branch master: commit 3</p>
           <p>git branch master: commit 4</p>
-          <p>git branch  developer: commit 1</p>
+          <p>git branch developer: commit 1</p>
           <p>git branch developer: commit 2</p>
           <p>git branch developer: commit 3</p>
         </header>
